192aa695d0f4622cc0e9d3caa77b9434542d44f9
Stricks1 <gmalheiross@gmail.com>
2020-08-28 18:39:19 -0300

    test start

---
+++ src/index.js
@@ -0,0 +1,42 @@
+ import {
+     SceneMain
+ } from "./scenes/sceneMain";
+ import {
+     SceneLoad
+ } from "./scenes/sceneLoad";
+ import {
+     SceneTitle
+ } from "./scenes/sceneTitle";
+ import {
+     SceneOver
+ } from "./scenes/sceneOver";
+  
+ //
+ //
+ //
+ var isMobile = navigator.userAgent.indexOf("Mobile");
+ if (isMobile == -1) {
+     isMobile = navigator.userAgent.indexOf("Tablet");
+ }
+ var w = 800;
+ var h = 640;
+ //
+ //
+ if (isMobile != -1) {
+     w = window.innerWidth;
+     h = window.innerHeight;
+ }
+ var config = {
+    type: Phaser.AUTO,
+    width: w,
+    height: h,
+    parent: 'phaser-game',
+    scene: [SceneLoad,SceneTitle,SceneMain,SceneOver],
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false
+      }
+    }
+ };
+ let game = new Phaser.Game(config);
